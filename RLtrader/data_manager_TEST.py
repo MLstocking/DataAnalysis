1d0a3e5d13c1f4364da4caf8c59e46a2f1d4719c
hlchoi <69101205+chl8469@users.noreply.github.com>
2020-10-27 21:37:43 +0900

    Delete data_manager_TEST.py

--- RLtrader/data_manager_TEST.py
+++
@@ -1,8 +0,0 @@
-import data_manager
-
-if __name__ == '__main__':
-    DBdata = data_manager.read_DBdata()
-    chart_data, training_data = data_manager.load_data(DBdata)
-
-    print(DBdata)
-    print(chart_data, training_data)
